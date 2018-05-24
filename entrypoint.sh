066dd0f14d6e82e66a3063bda01cbe0ff851c73f
Lewis Marshall <lewis.marshall@digital.homeoffice.gov.uk>
2018-05-24 15:07:59 +0100

    Pass params to watchdog

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,4 +7,4 @@ set -o pipefail
 echo "watchdog-timeout = ${IPMI_WATCHDOG_TIMEOUT:-180}">/etc/watchdog.d/watchdog-timeout.conf
 
 /usr/libexec/openipmi-helper start
-/usr/sbin/watchdog --foreground --config-file /etc/watchdog.conf --verbose
+/usr/sbin/watchdog --foreground --config-file /etc/watchdog.conf $@
